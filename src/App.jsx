8cdc47ce5297ac993851a590cfaa9ab89c31b490
alimardonov2317 <alimardonovsamandar707@gmail.com>
2025-01-28 12:10:51 +0500

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
-import Hero from './components/hero/Hero'
-import Products from './components/products/Products'
 import Footer from './components/footer/Footer'
+import Home from './components/pages/home/Home'
+import About from './components/pages/about/About'
+import Contact from './components/pages/contact/Contact'
+import Login from './components/pages/login/Login'
+import { Routes, Route, } from 'react-router-dom'
+
 
 function App() {
-  const [show, setShow] = React.useState(false)
- 
+
   return (
     <>
-      <Header/>
-      <Products/>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
 
 const Header = () => {
   return (
     <div className='bg-slate-500'>
       <nav className="container mx-auto h-20 shadow-md px-4 py-2 flex items-center justify-between mb-4">
         <div className="flex items-center">
-          <span className=" text-white ml-4 text-xl font-bold">Tailwind UI</span>
+          <Link to={"/"}><span className=" text-white ml-4 text-xl font-bold">Tailwind UI</span></Link>
         </div>
-        <input className='w-60 rounded-md px-2 py-1' type="text" name="" id="" placeholder='Saerch'/>
+        <ul className="flex gap-2">
+          <li>
+            <span><NavLink to={"/"}>Home</NavLink></span>
+          </li>
+          <li>
+            <span><NavLink to={"/about"}>About</NavLink></span>
+          </li>
+          <li>
+            <span><NavLink to={"/contact"}>Contact</NavLink>
+            </span>
+          </li>
+          <li>
+            <span><NavLink to={"/login"}>Login</NavLink>
+            </span>
+          </li>
+        </ul>
+        <input className='w-60 rounded-md px-2 py-1' type="text" name="" id="" placeholder='Saerch' />
         <div className="">
-          <button className="flex items-center px-3 py-2 border-2 text-md bg-gray-700 rounded-md text-white hover:text-gray-400 bg-gray-800 duration-300">Menu</button>
+          <button className="flex items-center px-3 py-2 border-2 text-md rounded-md text-white hover:text-gray-400 bg-gray-800 duration-300">Menu</button>
         </div>
       </nav>
     </div>
